9fe590091f16c8e699a7751efe319c0b575f3b1f
YYF <LalicID@outlook.com>
2018-01-02 13:02:10 +0800

    18.01.2修改

--- src/test/java/xyz/yyfeng/Test/TestUser.java
+++ src/test/java/xyz/yyfeng/Test/TestUser.java
@@ -1,6 +1,8 @@
-package xyz.yyfeng.test;
+package xyz.yyfeng.Test;
 
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.junit.Before;
 import org.junit.Test;
 import org.springframework.context.ApplicationContext;
@@ -8,17 +10,16 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 import xyz.yyfeng.mapper.UserMapper;
 import xyz.yyfeng.po.User;
 
-import java.lang.Exception;
-
 
 
 public class TestUser {
 
     private ApplicationContext applicationContext;
-
+    private static final Log log= LogFactory.getLog("logname");
     @Before
     public void setUP() throws Exception{
         applicationContext =new ClassPathXmlApplicationContext("classpath:config/spring/applicationContext.xml");
+
     }
     @Test
     public void selectUserinfo() {
